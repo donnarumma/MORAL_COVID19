ec86c4e54b8a198f40733bdd0be6154f3e6def54
Francesco Donnarumma <francesco.donnarumma@gmail.com>
2021-03-27 10:12:44 +0100

    On branch feature  Changes to be committed: 	modified:   DEMO_TRAVELLERS_CAMPANIA.m 	modified:   DEMO_TRAVELLERS_LAZIO.m 	modified:   DEMO_TRAVELLERS_LOMBARDIA.m 	modified:   Region_jumpers_fitting.m 	modified:   VARIABLES_TRAVELLERS.m
     Untracked files:
    	defauls_plot_params.m
    
    REVISION: new graphs update - 2021 03 27

--- DEMO_TRAVELLERS_LAZIO.m
+++ DEMO_TRAVELLERS_LAZIO.m
@@ -2,10 +2,16 @@
 VARIABLES_TRAVELLERS;
 addpath ('./DATA/');
 addpath ('ECheynet-SEIR');
+load ('si_42');
+input_params=defauls_plot_params;
+
+
 input_params.regionIdx  =INDEX_LAZIO;
-input_params.ifsave     =0;
-input_params.mode_plot  =THREE_PLOT;%DEATHS_PLOT;%   CHEATERS_PLOT;
-input_params.error_mode =NULL_MODEL;
-input_params.plot_legend=1;
+
+input_params.mode_plot  =TOTALS_PLOT;
+input_params.mode_plot  =DEATHS_PLOT;
+input_params.mode_plot  =ACTIVES_PLOT;
+% input_params.mode_plot  =THREE_PLOT;
+% input_params.mode_plot  =CHEATERS_PLOT;
 
 sim_struct_out=Region_jumpers_fitting(si,input_params);
--- DEMO_TRAVELLERS_LOMBARDIA.m
+++ DEMO_TRAVELLERS_LOMBARDIA.m
@@ -2,9 +2,17 @@
 VARIABLES_TRAVELLERS;
 addpath ('./DATA/');
 addpath ('ECheynet-SEIR');
+load ('si_42');
+
+input_params=defauls_plot_params;
+
 input_params.regionIdx  =INDEX_LOMBARDIA;
-input_params.ifsave     =0;
-input_params.mode_plot  =THREE_PLOT;
-input_params.error_mode =NULL_MODEL;
-input_params.plot_legend=1;
+
+input_params.ifsave     =1;
+input_params.mode_plot  =TOTALS_PLOT;
+input_params.mode_plot  =DEATHS_PLOT;
+input_params.mode_plot  =ACTIVES_PLOT;
+% input_params.mode_plot  =THREE_PLOT;
+% input_params.mode_plot  =CHEATERS_PLOT;
+
 sim_struct_out=Region_jumpers_fitting(si,input_params);
