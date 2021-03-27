d6427cbc353f935077e79b7d66303dbc5595bc5e
Francesco Donnarumma <francesco.donnarumma@gmail.com>
2021-03-27 10:23:28 +0100

    Add files via upload

---
+++ defauls_plot_params.m
@@ -0,0 +1,11 @@
+function input_params=defauls_plot_params()
+VARIABLES_TRAVELLERS;
+
+input_params.error_mode =NULL_MODEL;
+input_params.plot_legend=1; 
+input_params.mk         =MK;
+input_params.lw         =LW;
+input_params.cmaps      =CMAPS;
+input_params.ms         =MS;
+input_params.p          =POS;
+input_params.ifsave     =0;
